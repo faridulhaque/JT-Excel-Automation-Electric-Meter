4a48f3c11a51a4b8a8ee7abf6d57eea6df91cabb
= <=>
2026-01-21 23:11:43 +0600

    git add .

--- app/api/meter/route.ts
+++ app/api/meter/route.ts
@@ -37,8 +37,6 @@ export async function PUT(request: Request) {
   try {
     const body: TMeterData = await request.json();
 
-
-
     const updated = await prisma.meter.update({
       where: {
         id: body.id,
@@ -46,7 +44,6 @@ export async function PUT(request: Request) {
       data: {
         name: body.name,
         threshold: body.threshold,
-        
       },
     });
     if (updated)
@@ -55,6 +52,47 @@ export async function PUT(request: Request) {
         data: updated,
         message: "Meter updated successfully",
       });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const { id } = params;
+    const data = await prisma.meter.findMany({ where: { id } });
+    return NextResponse.json({
+      status: 200,
+      data: data,
+      message: "Data fetched successfully",
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const { id } = params;
+    const data = await prisma.meter.delete({ where: { id } });
+    
+    return NextResponse.json({
+      status: 200,
+      data: data,
+      message: "Data deleted successfully",
+    });
   } catch (error) {
     return NextResponse.json({
       status: 500,
