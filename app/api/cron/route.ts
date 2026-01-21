1ff54833c930fab0b987c04680d0c23e5504aab9
= <=>
2026-01-21 12:17:12 +0600

    workflow added

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
     console.log("api hit before authentiacation")
-  if (request.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
+//   if (request.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
 
   console.log('CRON HIT', new Date().toISOString());
 
