59390553e887b9c00ec61ebb6566806e54d71ddc
= <=>
2026-01-23 03:18:46 +0600

    dynamic params issue resolved 1

--- app/api/meter/[id]/route.ts
+++ app/api/meter/[id]/route.ts
@@ -4,29 +4,30 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
     const token = request.headers.get("authorization");
     if (!token)
-      return NextResponse.json({
-        status: 401,
-        message: "Unauthorized",
-      });
-    const { id } = jsonwebtoken.verify(
+      return NextResponse.json({ status: 401, message: "Unauthorized" });
+
+    const { id: userId } = jsonwebtoken.verify(
       token,
       process.env.JWT_SECRET as string,
     ) as { id: string };
 
+    const { id: meterId } = await params;
+
     const data = await prisma.meter.findFirst({
-      where: { userId: id, id: params?.id },
+      where: { userId, id: meterId },
     });
+
     return NextResponse.json({
       status: 200,
-      data: data,
+      data,
       message: "Data fetched successfully",
     });
-  } catch (error) {
+  } catch {
     return NextResponse.json({
       status: 500,
       message: "Internal server error",
